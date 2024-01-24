765f62093ba33a7f43b8f725795b79616317fabc
Brandon Herrera <brandondh769@gmail.com>
2024-01-24 12:35:40 -0600

    Update hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-import { Header } from './hero/hero'
+import { useState } from 'react';
+import Header from './hero/Header';
+import MainContent from './hero/MainContent';
 
-function App() {
+const navigation = [
+  { name: 'About', href: '#about' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Community', href: '#community' },
+  { name: 'Contact', href: '#contact' },
+];
+
+const App = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="hero-container">
-      <Header />
+    <div className="bg-white">
+      <Header navigation={navigation} mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+      <MainContent />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
